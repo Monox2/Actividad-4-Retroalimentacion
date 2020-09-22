f50818261d54a625ce3fbf7af8337d2d85a4adc3
Ramón García <ramon.garcia5800.alumnos.udg.mx>
2020-09-21 19:23:33 -0500

    El menu funciona, pero aun no tiene ninguna utilidad creada

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    char op;
+    do{
+        printf("1) Capturar enteros\n");
+        printf("2) Mostrar cadena n veces\n");
+        printf("3) Agregar personaje\n");
+        printf("0) Salir\n");
+        scanf("%c",&op);
+
+        switch (op)
+        {
+        case '1':
+            
+            break;
+        case '2':
+            
+            break;
+        case '3':
+            
+            break;
+        default:
+            
+            break;
+        }
+
+
+
+        fflush(stdin);
+    }while (op != '0');
+    return 0;
+}
